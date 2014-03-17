9075ebad64f53143ae2cfdb50ba82eeb2fe4060e
Diga Widyaprana <digawp@gmail.com>
2014-03-17 22:24:48 +0800

    Modify mergesort to be in place, testing even vs odd length of array

--- MergeSort.java
+++ MergeSort.java
@@ -4,47 +4,46 @@ import java.util.Arrays;
 
 public class MergeSort {
 
-	static <T extends Comparable<T>> void mergeSort(T[] A) {
-		if (A.length > 1) {
-			int q = A.length / 2;
+	static <T extends Comparable<T>> void mergeSort(T[] A, int lo, int hi) {
+		if (hi-lo > 1) {
+			int q = lo + (hi-lo)/2;
 
-			T[] leftArray = Arrays.copyOfRange(A, 0, q);
-			T[] rightArray = Arrays.copyOfRange(A, q, A.length);
+			mergeSort(A, lo, q);
+			mergeSort(A, q+1, hi);
 
-			mergeSort(leftArray);
-			mergeSort(rightArray);
-
-			merge(A, leftArray, rightArray);
+			merge(A, lo, q+1, hi+1);
 		}
 	}
 
-	static <T extends Comparable<T>>void merge(T[] a, T[] l, T[] r) {
-		int totElem = l.length + r.length;
+	static <T extends Comparable<T>>void merge(T[] a, int l, int r, int max) {
+		// int totElem = max - l;
 		// int[] a = new int[totElem];
-		int i, li, ri;
-		i = li = ri = 0;
-		while (i < totElem) {
-			if ((li < l.length) && (ri < r.length)) {
-				if (l[li].compareTo(r[ri]) < 0) {
-					a[i] = l[li];
+		T[] copy = a.clone();
+		int i = l;
+		int li = l;
+		int ri = r;
+		while (i < max) {
+			if ((li < r) && (ri < max)) {
+				if (copy[li].compareTo(copy[ri]) < 0) {
+					a[i] = copy[li];
 					i++;
 					li++;
 				} else {
-					a[i] = r[ri];
+					a[i] = copy[ri];
 					i++;
 					ri++;
 				}
 			} else {
-				if (li >= l.length) {
-					while (ri < r.length) {
-						a[i] = r[ri];
+				if (li >= r) {
+					while (ri < max) {
+						a[i] = copy[ri];
 						i++;
 						ri++;
 					}
 				}
-				if (ri >= r.length) {
-					while (li < l.length) {
-						a[i] = l[li];
+				if (ri >= max) {
+					while (li < r) {
+						a[i] = copy[li];
 						li++;
 						i++;
 					}
@@ -53,6 +52,14 @@ public class MergeSort {
 		}
 		// return a;
 
+	}
+	
+	public static void main(String[] args) {
+		Integer[] test = new Integer[]{1, 5, 3, 4};
+		mergeSort(test, 0, test.length);
+		for (int i = 0; i < test.length; i++) {
+			System.out.println(test[i]);
+		}
 	}
 
 }
