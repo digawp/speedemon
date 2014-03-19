d68044f6f00ca04bd6a5eb0aabcd77559dbe7144
Diga Widyaprana <digawp@gmail.com>
2014-03-19 19:23:03 +0800

    Cutting out some redundant codes, stable v1.1

--- SpeedDemon.java
+++ SpeedDemon.java
@@ -9,95 +9,137 @@ import java.util.Arrays;
 /**
  * 
  * @author Diga W
- *
+ * 
  */
 public class SpeedDemon {
 
 	/**
+	 * A pair of Integer[] to store the array of 128 integers representing an
+	 * ascii value each and an int representing its current count (number of
+	 * times a permutation of that characters appear in the database)
 	 * 
 	 * @author Diga W
-	 *
+	 * 
 	 */
 	class CustomPair {
-		
+
+		// the array of integer of size 128, each slot representing an ascii
+		// character, with value representing the number of times it appears in
+		// the line
 		Integer[] charList;
+
+		// the count for the number of times a permutation of the string
+		// represented by the array of integer above appears in the database
 		int total;
-		
+
+		/**
+		 * Constructor
+		 * 
+		 * @param charlist
+		 * @param value
+		 */
 		public CustomPair(Integer[] charlist, int value) {
 			charList = charlist;
 			total = value;
 		}
-		
+
+		/**
+		 * Returns the array of Integer
+		 * 
+		 * @return
+		 */
 		public Integer[] getCharList() {
 			return charList;
 		}
-		
+
+		/**
+		 * Returns the current count so far
+		 * 
+		 * @return
+		 */
 		public int getTotal() {
 			return total;
 		}
-		
+
+		/**
+		 * Increments the current count
+		 */
 		public void incrementTotal() {
 			total++;
 		}
-		
+
 	}
-	
+
+	// A buffered reader for reading the input file
 	BufferedReader reader;
+
+	// A hashmap to store the 'hashcode' and the custom pair class above
 	HashMap<Integer, CustomPair> hashMap = new HashMap<Integer, CustomPair>();
-	
+
+	/**
+	 * Constructor for SpeedDemon
+	 * 
+	 * @param path
+	 * @throws IOException
+	 */
 	public SpeedDemon(String path) throws IOException {
 		reader = new BufferedReader(new FileReader(path));
 	}
+
+	/**
+	 * Main function of SpeedDemon
+	 * 
+	 * @return The number of pairs
+	 * @throws IOException
+	 */
 	public int run() throws IOException {
-		
+
+		// The variable to be returned later as the answer
 		int answer = 0;
-		
+
+		// Reading the file
 		while (reader.ready()) {
 			String line = reader.readLine();
-			
+
+			// Calculates a 'hashcode' for the particular line (see the method
+			// definition for more info)
 			int noobHash = asciiCalc(line);
-			
+
+			// Generate an array of integer to represent the string
+			// Each ascii character has a slot, thus the size of array 128 (the
+			// fact that 10 and 13 being always 0 is ignored for simplicity)
 			Integer[] chars = new Integer[128];
-			
+
+			// Converts the string into the array of integer
 			for (int i = 0; i < line.length(); i++) {
 				int k = line.charAt(i);
-				if (chars[k-1] == null) {
-					chars[k-1] = 1;
+				if (chars[k - 1] == null) {
+					chars[k - 1] = 1;
 				} else {
 					chars[k - 1]++;
 				}
-				
+
 			}
-			
-			if (hashMap.containsKey(noobHash)) {
-				if (Arrays.deepEquals(hashMap.get(noobHash).getCharList(), chars)) {
+
+			while (hashMap.get(noobHash) != null) {
+				if (Arrays.deepEquals(hashMap.get(noobHash).getCharList(),
+						chars)) {
 					answer += hashMap.get(noobHash).getTotal();
 					hashMap.get(noobHash).incrementTotal();
-				} else {
-					while (hashMap.get(noobHash) != null) {
-						if (Arrays.deepEquals(hashMap.get(noobHash).getCharList(), chars)) {
-							answer += hashMap.get(noobHash).getTotal();
-							hashMap.get(noobHash).incrementTotal();
-							break;
-						}
-						noobHash++;
-					}
-					if (hashMap.get(noobHash) == null) {
-						hashMap.put(noobHash, new CustomPair(chars,1));
-					}
+					break;
 				}
-				
-				
-			} else {
+				noobHash++;
+			}
+			if (hashMap.get(noobHash) == null) {
 				hashMap.put(noobHash, new CustomPair(chars, 1));
 			}
-			
+
 		}
 		reader.close();
-		
+
 		return answer;
 	}
-	
+
 	static int asciiCalc(String s) {
 		int len = s.length();
 		int val = 0;
@@ -107,9 +149,9 @@ public class SpeedDemon {
 			} else {
 				val += s.charAt(i) * 803;
 			}
-			
+
 		}
 		return val;
 	}
-	
+
 }
